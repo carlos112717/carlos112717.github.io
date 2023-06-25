0cda0c50a7592b26047d431bb4721eeddce79f4b
carlos112717 <caauco@hotmail.com>
2023-06-25 18:38:17 -0500

    code in JavaScript form validation

--- validacion.js
+++ validacion.js
@@ -1,1 +1,24 @@
-//Haz tú validación en javascript acá
+//Validación formulario de envío
+function validarFormulario() {
+        // Obtener los valores ingresados por el usuario
+        var nombre = document.forms["form"]["nombre"].value;
+        var email = document.forms["form"]["email"].value;
+        var asunto = document.forms["form"]["asunto"].value;
+        var mensaje = document.forms["form"]["mensaje"].value;
+
+        // Validar que los campos no estén vacíos
+        if (nombre == "" || email == "" || asunto == "" || mensaje == "") {
+            alert("Por favor, completa todos los campos del formulario.");
+            return false; // Evita que el formulario se envíe
+        }
+
+        // Validar el formato del email usando una expresión regular
+        var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+            alert("Por favor, ingresa un email válido.");
+            return false; // Evita que el formulario se envíe
+        }
+
+        // Si todas las validaciones pasan, el formulario es válido
+        return true;
+    }
